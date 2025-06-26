7bd3cc78d31e6ea7284b5e829b852e3af70a1b33
Dharshan S <dharshans805@gmail.com>
2025-06-26 10:56:36 +0530

    dsa1

---
+++ src/DSA/linearSearch.java
@@ -0,0 +1,23 @@
+package DSA;
+
+public class linearSearch {
+    public static void main(String[] args) {
+        int[] nums = {2,-345,74,83,29,5,19,26,487,546};
+        int target=83;
+        int res = linearSearch(nums,target);
+        System.out.println("The "+target+" is present in Array at index "+ res);
+    }
+
+    static int linearSearch(int[] arr,int target){
+        if(arr.length == 0){
+            System.out.println("It's an Empty Array");
+        }
+
+        for (int index = 0; index < arr.length; index++) {
+            if(arr[index]==target){
+                return index;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/DSA/searchInString.java
@@ -0,0 +1,42 @@
+package DSA;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class searchInString {
+    public static void main(String[] args) {
+        String name = "Dacchu";
+        char target = 'h';
+//        System.out.println(searchString(name,target));
+        System.out.println(Arrays.toString(name.toCharArray())); //this will create a string array
+        System.out.println(searchString2(name,target));
+    }
+
+    static boolean searchString(String str,char target){
+        if(str.length()==0){
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i)==target){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean searchString2(String str,char target){
+        if(str.length()==0){
+            return false;
+        }
+
+        for(char ch:str.toCharArray()){  //char ch checks each elements so v shld convert the string into an array by using str.toCharArray()
+            if(ch==target){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
