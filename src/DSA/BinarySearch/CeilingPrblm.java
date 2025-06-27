3214745ae693962fa34dd58ab0abba934a509527
Dharshan S <dharshans805@gmail.com>
2025-06-27 11:30:57 +0530

    dsa2

---
+++ src/DSA/BinarySearch/CeilingPrblm.java
@@ -0,0 +1,29 @@
+package DSA.BinarySearch;
+
+public class CeilingPrblm {
+    public static void main(String[] args) {
+        int[] nums={1,3,5,9,14,16,18};
+        int target = 15;
+        System.out.println(bs(nums,target));
+    }
+
+    static int bs(int[] arr,int target){
+        int start= 0;
+        int end= arr.length-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(arr[mid]==target){
+                return mid;
+            }
+
+            if(target<arr[mid]){
+                end= mid-1;
+            } else{
+                start=mid+1;
+            }
+        }
+        return arr[end];
+    }
+}
