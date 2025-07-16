c68b964e14c39183409e4df0580687ea7f752ced
Dharshan S <dharshans805@gmail.com>
2025-07-16 18:08:23 +0530

    dsa14

---
+++ src/DSA/Sorting/FindAllDupli.java
@@ -0,0 +1,35 @@
+package DSA.Sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+// 442 same as finnding all missing num but here we will be return the element present that index instead of correct elemen
+
+public class FindAllDupli {
+    public List<Integer> findDuplicates(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                swap(nums,i,correct);
+            }else{
+                i++;
+            }
+        }
+
+        //finding missing
+        List<Integer> ans = new ArrayList<>();
+        for(int index=0;index<nums.length;index++){
+            if(nums[index]!= index+1){
+                ans.add (nums[index]);
+            }
+        }
+        return ans;
+    }
+    void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
+
+
