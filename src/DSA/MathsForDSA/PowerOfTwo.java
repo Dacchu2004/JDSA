f96ecf5438b310656c9c79706e701ea892023212
Dharshan S <dharshans805@gmail.com>
2025-07-23 20:00:30 +0530

    dsa19

---
+++ src/DSA/MathsForDSA/PowerOfTwo.java
@@ -0,0 +1,10 @@
+package DSA.MathsForDSA;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        int n = 16;
+        boolean ans = (n & (n-1))==0;
+        System.out.println(ans);
+    }
+
+}
