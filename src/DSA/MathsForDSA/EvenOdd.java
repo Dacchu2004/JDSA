3a4e39bb508f4517f18402d5c2d382a7b2d81eaf
Dharshan S <dharshans805@gmail.com>
2025-07-23 11:00:04 +0530

    dsa19

---
+++ src/DSA/MathsForDSA/EvenOdd.java
@@ -0,0 +1,11 @@
+package DSA.MathsForDSA;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+       int n =67;
+        System.out.println(isOdd(n));
+    }
+    static boolean isOdd(int n){
+        return (n&1)==1;
+    }
+}
