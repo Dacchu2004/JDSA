5f67933535512f27fbd1ff79799c86c2825c02c1
Dharshan S <dharshans805@gmail.com>
2025-07-25 11:08:10 +0530

    dsa21

---
+++ src/DSA/Arrays/Sieve.java
@@ -0,0 +1,25 @@
+package DSA.Arrays;
+
+public class Sieve {
+    public static void main(String[] args) {
+        int n = 40;
+        boolean[] primes = new boolean[n+1];
+        sieve(n,primes);
+    }
+
+    //false in arr means that number is prime
+    static void sieve(int n ,boolean[] primes){
+        for(int i =2; i*i<=n; i++){
+            if(!primes[i]){ // primes[i]==false
+                for (int j  = i*2; j<=n; j+=i){
+                    primes[j]=true;
+                }
+            }
+        }
+        for (int i = 2; i <=n ; i++) {
+            if(!primes[i]){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
