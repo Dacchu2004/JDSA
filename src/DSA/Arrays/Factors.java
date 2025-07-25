3fa453ff15a9effe1a440de0bc802f018b2c7dc5
Dharshan S <dharshans805@gmail.com>
2025-07-25 18:49:09 +0530

    dsa21

---
+++ src/DSA/Arrays/Factors.java
@@ -0,0 +1,26 @@
+package DSA.Arrays;
+
+import java.util.ArrayList;
+
+public class Factors {
+    public static void main(String[] args) {
+        factors(20);
+    }
+
+    static void factors(int n){
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if(n%i==0){
+                if(n/i==i){
+                    System.out.print(i+" ");
+                }else {
+                    System.out.print(i+" ");
+                    list.add(n/i);
+                }
+            }
+        }
+        for (int i = list.size()-1; i >=0 ; i--) { // coz the factors are stored in descending order in the arraylist
+            System.out.print(list.get(i)+" ");
+        }
+    }
+}
