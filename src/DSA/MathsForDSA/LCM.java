365b9c95e873dad2e08a5a9ac807eda423bdc3bf
Dharshan S <dharshans805@gmail.com>
2025-07-26 11:12:42 +0530

    dsa22

--- src/DSA/MathsForDSA/LCM.java
+++ src/DSA/MathsForDSA/LCM.java
@@ -1,13 +1,16 @@
 package DSA.MathsForDSA;
 
-public class EluclisdsGCD {
+public class LCM {
     public static void main(String[] args) {
-        System.out.println(gcd(4,9)); // 4 and 9 are co-primes
+        System.out.println(lcm(9,18));
     }
     static int gcd(int a , int b){
         if(a==0){
             return b;
         }
         return gcd(b%a,a);
+    }
+    static int lcm(int a , int b){
+        return a*b/(gcd(a,b));
     }
 }
