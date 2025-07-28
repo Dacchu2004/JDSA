dc1c5cf96f79e2bb106116759fef79e31342e339
Dharshan S <dharshans805@gmail.com>
2025-07-28 19:44:44 +0530

    dsa23

---
+++ src/DSA/Recurrsion/funBoth.java
@@ -0,0 +1,15 @@
+package DSA.Recurrsion;
+
+public class funBoth {
+    public static void main(String[] args) {
+        funBoth(5);
+    }
+    static void funBoth(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        funBoth(n-1);
+        System.out.println(n);
+    }
+}
