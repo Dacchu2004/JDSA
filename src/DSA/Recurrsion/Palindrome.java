8b292baeea0aa65c85b96899eff19f565f42e211
Dharshan S <dharshans805@gmail.com>
2025-07-29 11:22:56 +0530

    dsa24

---
+++ src/DSA/Recurrsion/Palindrome.java
@@ -0,0 +1,21 @@
+package DSA.Recurrsion;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(palind(1321));
+    }
+    static int rev(int n){
+        int digits = (int)(Math.log10(n)+1);
+        return helper(n, digits);
+    }
+    static int helper(int n,int digits){
+        if(n%10==n){
+            return n;
+        }
+        int rem = n %10;
+        return rem *(int) (Math.pow(10,digits-1))+helper(n/10,digits-1);
+    }
+    static boolean palind(int n){
+        return n == rev(n);
+    }
+}
