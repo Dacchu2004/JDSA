f8337db14f062fbafde6e231f1fed3605a9d0daa
Dharshan S <dharshans805@gmail.com>
2025-07-29 10:47:50 +0530

    dsa24

---
+++ src/DSA/Recurrsion/ReverseNum1.java
@@ -0,0 +1,18 @@
+package DSA.Recurrsion;
+
+public class ReverseNum1{
+    static int sum = 0;
+    static void rev1(int n){
+        if(n == 0){
+            return;
+        }
+        int rem = n%10;
+        sum = sum * 10 + rem;
+        rev1(n/10);
+    }
+
+    public static void main(String[] args) {
+        rev1(1682);
+        System.out.println(sum);
+    }
+}
---
+++ src/DSA/Recurrsion/ReverseNum2.java
@@ -0,0 +1,18 @@
+package DSA.Recurrsion;
+
+public class ReverseNum2 {
+    public static void main(String[] args) {
+        System.out.println(rev2(1682));
+    }
+    static int rev2(int n){
+        int digits = (int)(Math.log10(n)+1);
+        return helper(n, digits);
+    }
+    static int helper(int n,int digits){
+        if(n%10==n){
+            return n;
+        }
+        int rem = n %10;
+        return rem *(int) (Math.pow(10,digits-1))+helper(n/10,digits-1);
+    }
+}
