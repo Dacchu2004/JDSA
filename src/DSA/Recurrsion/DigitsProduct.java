4fb8d316070fa9840865c681451851f9085d01f1
Dharshan S <dharshans805@gmail.com>
2025-07-29 09:42:42 +0530

    dsa24

---
+++ src/DSA/Recurrsion/DigitsProduct.java
@@ -0,0 +1,14 @@
+package DSA.Recurrsion;
+
+public class DigitsProduct {
+    public static void main(String[] args) {
+        int ans = product(2004);
+        System.out.println(ans);
+    }
+    static int product(int n){
+        if((n%10)==n){
+            return n;
+        }
+        return (n%10)* product(n/10);
+    }
+}
