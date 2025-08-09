2021bbcdfd2ec1b2ab02b2b2daa3a39c4d4c56bb
Dharshan S <dharshans805@gmail.com>
2025-08-09 21:08:33 +0530

    dsa32

---
+++ src/DSA/StringRecursion/ASCII.java
@@ -0,0 +1,8 @@
+package DSA.StringRecursion;
+
+public class ASCII{
+    public static void main(String[] args) {
+        char ch = 'a';
+        System.out.println(ch+0);
+    }
+}
--- src/DSA/StringRecursion/SubSequence.java
+++ src/DSA/StringRecursion/SubSequence.java
@@ -1,8 +1,11 @@
 package DSA.StringRecursion;
 
+import java.util.ArrayList;
+
 public class SubSequence {
     public static void main(String[] args) {
         subseq("","abc");
+        System.out.println(subseq1("","abc"));
     }
     static void subseq(String p, String up){
         if(up.isEmpty()){
@@ -11,7 +14,22 @@ public class SubSequence {
         }
 
         char ch = up.charAt(0);
-        subseq(p+ch,up.substring(1));
+        subseq(p+ch,up.substring(1)); // strings are immutable so the up.substring will always be creating an new object
         subseq(p,up.substring(1));
+    }
+
+    //returning an arraylist of string but passing the arraylist inside the function body itself
+    static ArrayList<String> subseq1(String p,String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> left = subseq1(p+ch,up.substring(1));
+        ArrayList<String> right = subseq1(p,up.substring(1));
+
+        left.addAll(right);
+        return left;
     }
 }
