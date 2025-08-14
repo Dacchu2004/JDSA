d2f9c93d3dc7591b46dd35bc51ebb3db24a7fef0
Dharshan S <dharshans805@gmail.com>
2025-08-14 07:46:33 +0530

    dsa36

--- src/DSA/SlidingWindow/MaxAvg.java
+++ src/DSA/SlidingWindow/MaxAvg.java
@@ -1,6 +1,6 @@
-package DSA;
+package DSA.SlidingWindow;
 
-public class SlidingWindow {
+public class MaxAvg {
     public static void main(String[] args) {
         int [] nums = {1,12,-5,-6,50,3};
         int k = 4;
@@ -16,8 +16,8 @@ public class SlidingWindow {
         double maxSum = sum;
 
         //Slide the window
-        for (int i = k; i < nums.length; i++) {
-            sum += nums[i] - nums[i - k];  // add new, remove old
+        for (int j = k; j < nums.length; j++) {
+            sum += nums[j] - nums[j - k];  // add new, remove old
             maxSum = Math.max(maxSum, sum);
         }
         return maxSum / k; // average
